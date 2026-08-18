23166ee897879f298f5a97eda6d4358158aa0020
inval <inval@INVUZ>
2026-08-18 18:33:54 +0200

    Aggiungere i file di progetto.

---
+++ Battaglia_Navale_Lasku_Pagliuca/Form1.cs
@@ -0,0 +1,10 @@
+namespace Battaglia_Navale_Lasku_Pagliuca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
