7bafe5d211c6bea62fa207393954f4a22b20eed9
no-longer-human-3 <invalasku@gmail.com>
2026-08-30 18:51:49 +0200

    inizio codice

--- Battaglia_Navale_Lasku_Pagliuca/Form1.cs
+++ Battaglia_Navale_Lasku_Pagliuca/Form1.cs
@@ -6,7 +6,21 @@ namespace Battaglia_Navale_Lasku_Pagliuca
         {
             InitializeComponent();
         }
+    }
 
-        
+    public struct Posizione // Struttura per rappresentare la posizione di un quadratino
+    {
+        public int Riga;
+        public int Colonna;
+    }
+
+    public struct Nave
+    {
+        public string nome;
+        public int dim;
+        public Posizione[] Coord;
+        public bool[] colpiSubiti;
+        public bool affondo;
+
     }
 }
