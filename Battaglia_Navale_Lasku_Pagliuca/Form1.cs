fd305d57f98ade66314707390b5c0cb86991b30d
no-longer-human-3 <invalasku@gmail.com>
2026-09-01 22:55:51 +0200

    fine funzione 2 (credo che abbia bisogno di qualche miglioramento)

--- Battaglia_Navale_Lasku_Pagliuca/Form1.cs
+++ Battaglia_Navale_Lasku_Pagliuca/Form1.cs
@@ -13,11 +13,7 @@ namespace Battaglia_Navale_Lasku_Pagliuca
         int[,] mioCampo = new int[righe, colonne];
         Nave[] miaFlotta = new Nave[navi];
 
-        // ==========================================
-        // PARTE DEL PROGRAMMATORE 1
-        // ==========================================
-
-        private void InizializzaFlotta()
+        private void InizializzaFlotta() // Funzione per inizializzare la flotta
         {
             // Dichiarazione Portaerei
             miaFlotta[0].nome = "Portaerei";
@@ -53,21 +49,65 @@ namespace Battaglia_Navale_Lasku_Pagliuca
             miaFlotta[4].Coord = new Posizione[2];
             miaFlotta[4].colpiSubiti = new bool[2];
             miaFlotta[4].affondo = false;
+
             // 4. PosizionaNave(...)
             // 6. ControllaAffondato(...)
 
-
-            // ==========================================
-            // PARTE DEL PROGRAMMATORE 2
-            // ==========================================
-
-            // 2. VerificaCoordinate(...)
-            // 3. ConvertiCoordinate(...)
-            // 5. GestisciColpo(...)
-
         }
 
-    public struct Posizione // Struttura per rappresentare la posizione di un quadratino
+        private bool VerificaCoordinate(string coordinate) // Funzione per verificare le coordinate inserite dall'utente
+        {
+           
+            if (coordinate.Length < 2 || coordinate.Length > 3)
+            {
+                return false;
+            }
+            char[] charValide = { 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'L', 'l' };
+            bool charValida = false;
+
+            
+            for (int i = 0; i < charValide.Length; i++)
+            {
+                if (coordinate[0] == charValide[i])
+                {
+                    charValida = true;
+                    break; 
+                }
+            }
+
+            
+            if (charValida == false)
+            {
+                return false;
+            }
+
+          
+            if (coordinate.Length == 2)
+            {
+                if (coordinate[1] >= '1' && coordinate[1] <= '9')
+                {
+                    return true;
+                }
+            }
+
+            
+            if (coordinate.Length == 3)
+            {
+                if (coordinate[1] == '1' && coordinate[2] == '0')
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
+
+
+            // 3. ConvertiCoordinate(...)
+            // 5. GestisciColpo(...)
+        }
+
+        public struct Posizione // Struttura per rappresentare la posizione di un quadratino
     {
         public int Riga;
         public int Colonna;
